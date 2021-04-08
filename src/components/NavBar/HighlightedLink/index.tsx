efdc626e599ee7864b0895b3b42422d8384e3e5b
Abhinav Pappu <abhi1pappu9@gmail.com>
2021-04-08 13:18:00 -0500

    Add nav bar (#38)
    * Add initial version of Nav Bar
    
    * Complete nav bar
    
    * Comment out pages

---
+++ src/components/NavBar/HighlightedLink/index.tsx
@@ -0,0 +1,30 @@
+import clsx from 'clsx';
+import React, { ReactNode } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import styles from './styles.module.scss';
+
+type PropTypes = {
+  to: string;
+  children: ReactNode;
+  color?: string;
+  highlightColor?: string;
+  className?: string;
+  style?: React.CSSProperties;
+  [key: string]: unknown;
+};
+
+const HighlightedLink = ({ to, children, color = 'white', highlightColor = '#EE6C72', className, style, ...props }: PropTypes): JSX.Element => (
+  <NavLink
+    className={clsx(styles.highlightedLink, className)}
+    to={to}
+    style={{ color, ...style }}
+    {...props}
+    activeClassName={styles.active}
+  >
+    {children}
+    <div className={styles.highlight} style={{ backgroundColor: highlightColor }} />
+  </NavLink>
+);
+
+export default HighlightedLink;
