d6b7c75977b955c729754f06e707e622b543ec2b
Nick King <nrking0@gmail.com>
2021-11-19 02:07:39 -0600

    Create basic oven light design for registration

--- src/pages/Registration/Navigation/Fixtures.tsx
+++ src/pages/Registration/Navigation/Fixtures.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import clsx from 'clsx';
-// import useWindowSize from 'util/useWindowSize';
 
 import styles from './styles.module.scss';
+import LIGHT from '../../../assets/registration/navigation/light.svg';
+import LIGHT_ON from '../../../assets/registration/navigation/activatedLight.svg';
 
 type Props = {
   setFormIndex: React.Dispatch<React.SetStateAction<number>>,
@@ -21,47 +25,25 @@ const lights: [name: string, cx: number, cy: number, r: number, textx: number][]
 const finishScreenIndex = 5;
 
 const Fixtures = ({ setFormIndex, formIndex }: Props): JSX.Element => {
-  // const [, scx, scy, sr] = lights[formIndex];
-
   // we don't want the light for the Finish page to be clickable, and if we're on the Finish page nothing should be clickable
   const isDisabled = (i: number) => (i === finishScreenIndex) || (formIndex === finishScreenIndex);
 
-  // const { width, height } = useWindowSize();
-
   return (
-    <div>
-      <svg width="4321" height="113" viewBox="0 0 4321 120" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.fixtures}>
-        {lights.map(([name, cx, cy, r, textx], i) => (
-          <React.Fragment key={name}>
-            <g className={clsx(styles.lightGroup, isDisabled(i) && styles.disabled)} onClick={() => !isDisabled(i) && setFormIndex(i)}>
-              <circle
-                // style={{ position: 'absolute', left: '100' }}
-                cx={cx}
-                cy="120%"
-                r={r}
-                fill={i === formIndex ? '#FBB626' : '#85692F'}
-                stroke="black"
-                strokeWidth="2.25px"
-              />
-              <text className={styles.text} style={{ '--length': name.length } as any} x={textx} y="185" fill="black">{name}</text>
-            </g>
-
-            {i === formIndex && (
-              <g filter="url(#filter0_f)">
-                <circle cx={cx} cy={cy} r={r} fill="#FED684" />
-              </g>
-            )}
-          </React.Fragment>
-        ))}
-
-        {/* <defs>
-          <filter id="filter0_f" x={scx - (sr + 15)} y={scy - (sr + 15)} width={2 * sr + 30} height={2 * sr + 30} filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
-            <feGaussianBlur stdDeviation="7.5" result="effect1_foregroundBlur" />
-          </filter>
-        </defs> */}
-      </svg>
+    <div className={styles.ovenLights}>
+      {lights.map(([name], i) => (
+        <div className={clsx(styles.lightGroup, isDisabled(i) && styles.disabled)} onClick={() => !isDisabled(i) && setFormIndex(i)}>
+          {i === formIndex ? (
+            <div className={styles.image}>
+              <img className={styles.image} src={LIGHT_ON} alt="" />
+            </div>
+          ) : (
+            <div className={styles.image}>
+              <img src={LIGHT} alt="" />
+            </div>
+          )}
+          <p>{name}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/pages/Registration/Navigation/index.tsx
+++ src/pages/Registration/Navigation/index.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 
 import Fixtures from './Fixtures';
 
-import styles from './styles.module.scss';
-
 type Props = {
   setFormIndex: React.Dispatch<React.SetStateAction<number>>,
   formIndex: number
 };
 
 const Navigation = ({ setFormIndex, formIndex }: Props): JSX.Element => (
-  // <div className={styles.navigation}>
-  //   <Fixtures setFormIndex={setFormIndex} formIndex={formIndex} />
-  // </div>
   <div>
-    {/* <Fixtures setFormIndex={setFormIndex} formIndex={formIndex} /> */}
+    <Fixtures setFormIndex={setFormIndex} formIndex={formIndex} />
   </div>
 );
 
