e43a83326e0110dcf314c87ad55ef6b01b767d9b
Aniket Gargya <gargya.aniket@gmail.com>
2022-01-06 19:17:19 +0530

    :bug: remove unused imports

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import Hero from './Hero';
 import EventInfo from './EventInfo';
 import Sponsors from './Sponsors';
-import Speakers from './Speakers';
-import OurTeam from './OurTeam';
+// import Speakers from './Speakers';
+// import OurTeam from './OurTeam';
 // import Footer from './Footer';
 
 import styles from './styles.module.scss';
