b96a1cb00553d0e3f42b6d82805186553d986241
Aniket Gargya <gargya.aniket@gmail.com>
2022-01-10 21:26:04 +0530

    :fire: delete old sponsors code

--- src/pages/Home/Sponsors/index.tsx
+++ src/pages/Home/Sponsors/index.tsx
@@ -15,21 +15,6 @@ const Sponsors = (): JSX.Element => (
         <p className={`${styles.interested} ${styles.imageText}`}>Interested to sponsor HackIllinois 2022,<br />Email us at <a href="mailto:sponsor@hackillinois.org">sponsor@hackillinois.org</a></p>
       </div>
     </div>
-    {/* <img className={styles.leftTrees} src={LEFT_TREES} alt="" />
-    <img className={styles.rightTrees} src={RIGHT_TREES} alt="" />
-    <img className={styles.bottomTrees} src={BOTTOM_TREES} alt="" />
-
-    <h2 className={styles.title}>Sponsors</h2>
-
-    {sponsors.map((sponsorList, i) => (
-      <div className={clsx(styles.sponsorSection, i === 0 && styles.first)}>
-        {sponsorList.map(({ name, image, link }) => (
-          <a href={link} className={styles.sponsor} style={{ backgroundImage: `url("/assets/sponsor_logos/${image}")` }} target="_blank" rel="noopener noreferrer">
-            <span className={styles.srOnly}>{name}</span>
-          </a>
-        ))}
-      </div>
-    ))} */}
   </section>
 );
 
