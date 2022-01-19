8a5700fb35f41cef189baaeacc67e219d3003305
estelle0500 <estelle0500@gmail.com>
2022-01-18 23:45:23 -0600

    Fix sponsor display using scalable middle section

--- src/pages/Home/Sponsors/index.tsx
+++ src/pages/Home/Sponsors/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-import MENU from 'assets/home/sponsors/menu.svg';
 import DISPLAY_CARD from 'assets/home/sponsors/display_card.svg';
+import MENU_TOP from 'assets/home/sponsors/menu_top.svg';
+import MENU_MIDDLE from 'assets/home/sponsors/menu_middle.svg';
+import MENU_BOTTOM from 'assets/home/sponsors/menu_bottom.svg';
+import TITLE from 'assets/home/sponsors/title.svg';
+
 import { TITLE_SPONSORS, SPONSORS } from './sponsors';
 
 import styles from './styles.module.scss';
@@ -9,10 +13,18 @@ import styles from './styles.module.scss';
 const Sponsors = (): JSX.Element => (
   <section className={styles.sponsors}>
     <div className={styles.menuWrapper}>
-      <img className={styles.menu} src={MENU} alt="" />
-      <h2 className={styles.ourSponsors}>Our Sponsors</h2>
-      <div className={styles.menuContent}>
-        <p>Interested to sponsor HackIllinois 2022,<br />Email us at <a href="mailto:sponsor@hackillinois.org">sponsor@hackillinois.org</a></p>
+      <img src={MENU_TOP} alt="" />
+      <div className={styles.menuContent} style={{ backgroundImage: `url(${MENU_MIDDLE}` }}>
+        <div className={styles.title}>
+          <img src={TITLE} alt="" />
+          <div className={styles.titleTextContainer}>
+            <h2> Our Sponsors </h2>
+          </div>
+        </div>
+        <p>
+          Interested to sponsor HackIllinois 2022?<br />
+          Email us at <a href="mailto:sponsor@hackillinois.org">sponsor@hackillinois.org</a>
+        </p>
         <div className={styles.sponsorCards}>
           { TITLE_SPONSORS.map(({ name, image, link }) => (
             <div className={styles.titleSponsorCardContainer}>
@@ -22,13 +34,14 @@ const Sponsors = (): JSX.Element => (
           ))}
 
           { SPONSORS.map(({ name, image, link }) => (
-            <div className={styles.sponsorCardContainer}>
+            <div className={styles.sponsorCardContainer} key={name}>
               <img src={DISPLAY_CARD} alt="" className={styles.sponsorCard} />
               <a href={link}><img className={styles.sponsorLogo} src={`${process.env.PUBLIC_URL}/assets/sponsor_logos/${image}`} alt={name} /></a>
             </div>
           ))}
         </div>
       </div>
+      <img src={MENU_BOTTOM} alt="" />
     </div>
   </section>
 );
