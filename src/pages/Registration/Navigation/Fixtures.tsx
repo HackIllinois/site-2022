e800dd8d5ecb83780cfed666ad077d6bcbf8d479
Nick King <nrking0@gmail.com>
2022-01-19 18:02:35 -0600

    Update Fixtures to new Theme

--- src/pages/Registration/Navigation/Fixtures.tsx
+++ src/pages/Registration/Navigation/Fixtures.tsx
@@ -16,13 +16,14 @@ type Props = {
 const lights: [name: string, cx: number, cy: number, r: number, textx: number][] = [
   ['Welcome', 1639.5, 120, 18, 1602.5],
   ['Personal Info', 1817, 120, 18, 1760],
-  ['Race Demographics', 1995, 120, 18, 1915],
+  ['Demographics', 1995, 120, 18, 1915],
   ['Education', 2276.5, 120, 18, 2236.5],
   ['Experience', 2496.5, 120, 18, 2454.5],
+  ['Event Questions', 2496.5, 120, 18, 2454.5],
   ['Finish', 2705.5, 120, 18, 2679.5],
 ];
 
-const finishScreenIndex = 5;
+const finishScreenIndex = 6;
 
 const Fixtures = ({ setFormIndex, formIndex }: Props): JSX.Element => {
   // we don't want the light for the Finish page to be clickable, and if we're on the Finish page nothing should be clickable
