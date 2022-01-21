35fc19ab546688bf2bc3ffcd963695530699e2ee
Nick King <nrking0@gmail.com>
2022-01-20 22:45:41 -0600

    Add back home button

--- src/pages/Registration/Form/screens/Finish.tsx
+++ src/pages/Registration/Form/screens/Finish.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 
+import Button from 'components/form/Button';
 import LOGO_LARGE from 'assets/registration/finishLogo.svg';
 import styles from './styles.module.scss';
 
@@ -11,6 +12,10 @@ const Finish = (): JSX.Element => (
     </a>
 
     <p className={styles.text}>Thank you for registering for HackIllinois 2022! Be sure to follow our <a href="https://www.instagram.com/hackillinois/" target="_blank" rel="noreferrer">instagram (@hackillinois)</a> and our <a href="https://twitter.com/hackillinois/" target="_blank" rel="noreferrer">twitter (@hackillinois)</a>. We will be posting live updates during the event that you won’t want to miss!</p>
+
+    <div className={styles.homeButton}>
+      <Button onClick={() => { window.location.href = '/'; }}>Back Home</Button>
+    </div>
   </div>
 );
 
