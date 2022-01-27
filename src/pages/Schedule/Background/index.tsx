fe4ab36c4ab2b19ad268f3d8e892ed27c8e4f5e8
Felicia Wang <felicia5@illinois.edu>
2022-01-26 20:55:44 -0600

    Commit miscellaneous changes

--- src/pages/Schedule/Background/index.tsx
+++ src/pages/Schedule/Background/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import SCHEDULE_BG from 'assets/schedule/background.svg';
-// import DISPLAY from 'assets/schedule/display.svg';
+import DISPLAY from 'assets/schedule/display.svg';
 import styles from './styles.module.scss';
 
 const Background: React.FC = () => (
   <div className={styles.background} style={{ backgroundImage: `url("${SCHEDULE_BG}")` }}>
-    {/* <img src={DISPLAY} alt="Pastry display" className={styles.display} /> */}
+    
   </div>
 );
 
