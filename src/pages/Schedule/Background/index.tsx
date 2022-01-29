92c92e9cd04476ea0d374171986f613172202058
Felicia Wang <felicia5@illinois.edu>
2022-01-29 15:49:10 -0600

    Commit miscellaneous

--- src/pages/Schedule/Background/index.tsx
+++ src/pages/Schedule/Background/index.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import STARS from 'assets/schedule/stars.svg';
-import LEFT_TREES from 'assets/schedule/left_trees.svg';
-import RIGHT_TREES from 'assets/schedule/right_trees.svg';
-import FRONT_TREE from 'assets/schedule/front_tree.svg';
+import SCHEDULE_BG from 'assets/schedule/background.svg';
 import styles from './styles.module.scss';
 
 const Background: React.FC = () => (
-  <div className={styles.background}>
-    <div className={styles.stars} style={{ backgroundImage: `url("${STARS}")` }} />
-    <div className={styles.leftTrees} style={{ backgroundImage: `url("${LEFT_TREES}")` }} />
-    <div className={styles.rightTrees} style={{ backgroundImage: `url("${RIGHT_TREES}")` }} />
-    <div className={styles.frontTree} style={{ backgroundImage: `url("${FRONT_TREE}")` }} />
+  <div className={styles.background} style={{ backgroundImage: `url("${SCHEDULE_BG}")` }}>
+
   </div>
 );
 
