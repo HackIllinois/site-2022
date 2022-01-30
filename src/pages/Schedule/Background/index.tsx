4b1e6c244266f7866dc4c67d0f627d78d305dfe1
Felicia Wang <felicia5@illinois.edu>
2022-01-29 20:24:17 -0600

    Revert to original schedule page to prevent merge conflicts

--- src/pages/Schedule/Background/index.tsx
+++ src/pages/Schedule/Background/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
-import SCHEDULE_BG from 'assets/schedule/background.svg';
-import DISPLAY from 'assets/schedule/display.svg';
+import STARS from 'assets/schedule/stars.svg';
+import LEFT_TREES from 'assets/schedule/left_trees.svg';
+import RIGHT_TREES from 'assets/schedule/right_trees.svg';
+import FRONT_TREE from 'assets/schedule/front_tree.svg';
 import styles from './styles.module.scss';
 
 const Background: React.FC = () => (
-  <div className={styles.background} style={{ backgroundImage: `url("${SCHEDULE_BG}")` }}>
-    
+  <div className={styles.background}>
+    <div className={styles.stars} style={{ backgroundImage: `url("${STARS}")` }} />
+    <div className={styles.leftTrees} style={{ backgroundImage: `url("${LEFT_TREES}")` }} />
+    <div className={styles.rightTrees} style={{ backgroundImage: `url("${RIGHT_TREES}")` }} />
+    <div className={styles.frontTree} style={{ backgroundImage: `url("${FRONT_TREE}")` }} />
   </div>
 );
 
