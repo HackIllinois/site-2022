3df483c1b87adec03115121451e7e8eaaddcb63d
Felicia Wang <felicia5@illinois.edu>
2022-01-30 18:03:59 -0600

    Initial background created

--- src/pages/Schedule/Background/index.tsx
+++ src/pages/Schedule/Background/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-import SCHEDULE_BG from 'assets/schedule/background.svg';
+import DISPLAY_HALF from 'assets/schedule/display_half.svg';
+import DISPLAY_FULL from 'assets/schedule/display_full.svg';
+import SHELF from 'assets/schedule/shelf.svg';
 import styles from './styles.module.scss';
 
 const Background: React.FC = () => (
-  <div className={styles.background} style={{ backgroundImage: `url("${SCHEDULE_BG}")` }}>
-
+  <div className={styles.background}>
+    <div className={styles.wall}/>
+    <div className={styles.floor_separator}/>
+    <img src={DISPLAY_HALF} alt="Pastry display" className={styles.display_half} />
+    <img src={DISPLAY_FULL} alt="Pastry display" className={styles.display_full}/>
+    <img src={SHELF} alt="Shelf" className={styles.shelf} />
   </div>
 );
 
