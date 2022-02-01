aa49de8cc053cd7b09db285d1a046f90fb5ce694
Aniket Gargya <gargya.aniket@gmail.com>
2022-02-01 15:28:30 -0600

    :sparkles: add cupcake section (without carousel)

--- src/pages/Home/Speakers/index.tsx
+++ src/pages/Home/Speakers/index.tsx
@@ -9,7 +9,14 @@ const Speakers: React.FC = () => (
   <section className={styles.speakers}>
     <div className={styles.cupcakeStandContainer}>
       <img className={styles.cupcakeStand} src={CUPCAKE_STAND} alt="" />
-      <img className={styles.cupcake} src={CUPCAKE} alt="" />
+      <div className={styles.cupcakeStandContent}>
+        <img className={styles.cupcake} src={CUPCAKE} alt="" />
+        <div className={styles.cupcakeStandText}>
+          <h2>Speaker Name</h2>
+          <h3>Speaker info & Position</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sapiente quod laborum corporis est. Id ipsum dolore rem nobis sint ratione quia deserunt corrupti, rerum quasi ipsam at soluta quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo qui rerum assumenda dicta explicabo quos quas ut minus voluptatum, eum libero harum dolorem, voluptate laborum adipisci excepturi eligendi, veniam quo.</p>
+        </div>
+      </div>
     </div>
   </section>
 );
