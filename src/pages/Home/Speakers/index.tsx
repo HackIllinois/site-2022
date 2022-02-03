b210adfdc6d1bb3d37095b5fa59af4b6f5bb0b10
Aniket Gargya <gargya.aniket@gmail.com>
2022-02-03 14:15:44 -0600

    :construction: add speaker section updates

--- src/pages/Home/Speakers/index.tsx
+++ src/pages/Home/Speakers/index.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CUPCAKE_STAND from 'assets/home/speakers/cupcake_stand.svg';
 import CUPCAKE from 'assets/home/speakers/cupcake.svg';
 
+import CAROUSEL_BUTTON_SELECTED from 'assets/home/speakers/carousel_button_selected.svg';
+import CAROUSEL_BUTTON_UNSELECTED from 'assets/home/speakers/carousel_button_unselected.svg';
+
+import speakerData from './speakerData';
+
 import styles from './styles.module.scss';
 
-const Speakers: React.FC = () => (
-  <section className={styles.speakers}>
-    <div className={styles.cupcakeStandContainer}>
-      <img className={styles.cupcakeStand} src={CUPCAKE_STAND} alt="" />
-      <div className={styles.cupcakeStandContent}>
-        <img className={styles.cupcake} src={CUPCAKE} alt="" />
-        <div className={styles.cupcakeStandText}>
-          <h2>Speaker Name</h2>
-          <h3>Speaker info & Position</h3>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sapiente quod laborum corporis est. Id ipsum dolore rem nobis sint ratione quia deserunt corrupti, rerum quasi ipsam at soluta quis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo qui rerum assumenda dicta explicabo quos quas ut minus voluptatum, eum libero harum dolorem, voluptate laborum adipisci excepturi eligendi, veniam quo.</p>
+const Speakers: React.FC = () => {
+  const [speakerIndex, setSpeakerIndex] = useState(0);
+
+  return (
+    <section className={styles.speakers}>
+      <div className={styles.cupcakeStandContainer}>
+        <img className={styles.cupcakeStand} src={CUPCAKE_STAND} alt="" />
+        <div className={styles.cupcakeStandContent}>
+          <img className={styles.cupcake} src={CUPCAKE} alt="" />
+          <div className={styles.cupcakeStandText}>
+            <h2>{speakerData[speakerIndex].name}</h2>
+            <h3>{speakerData[speakerIndex].infoAndPosition}</h3>
+            <p>{speakerData[speakerIndex].description}</p>
+          </div>
         </div>
+        <div className={styles.carouselButtons}>
+          {Array(speakerData.length).fill(0).map((_, i) => i).map((i) => (
+            <img
+              src={i == speakerIndex ? CAROUSEL_BUTTON_SELECTED : CAROUSEL_BUTTON_UNSELECTED}
+              className={styles.carouselButton}
+              onClick={() => setSpeakerIndex(i)}
+            />
+          ))}
+          </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
 
 export default Speakers;
