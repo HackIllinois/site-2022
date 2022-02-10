497a00841c2bf37f141c6c60b8af65bfd855852c
Aniket Gargya <gargya.aniket@gmail.com>
2022-02-09 18:55:50 -0600

    :sparkles: make general faqs using piping thing

--- src/pages/Home/EventInfo/faqs.ts
+++ src/pages/Home/EventInfo/faqs.ts
@@ -1,76 +1,114 @@
-const FAQS = [
+const faqs = [
   {
-    question: 'What is a hackathon?',
-    answer: [
-      'A hackathon is a collaborative event in which teams learn new skills & technologies and create projects. There’s prizes to win, workshops on various fields in Computer Science, and other fun mini-events to take part in! This year, HackIllinois will be a 36-hour virtual hackathon, meaning that attendees from around the world can attend from the comfort of their home.',
+    sectionTitle: 'General',
+    sectionFaqs: [
+      {
+        question: 'What',
+        answer: [
+          'HackIllinois is a 36-hour event where college students from across the country work on open source projects, including new features, documentation, and bug fixes. You can work on contributing to a useful open source tool that you use, an open source project of your own, or joining a team led by an open source mentor.',
+        ],
+      },
+      {
+        question: 'Where',
+        answer: [
+          'HackIllinois will occur in multiple buildings on the University of Illinois at Urbana-Champaign campus, primarily at the Siebel Center for Computer Science (201 N Goodwin Ave, Urbana, IL 61801) and Electrical & Computer Engineering Building (306 N Wright St, Urbana, IL 61801).',
+        ],
+      },
+      {
+        question: 'When',
+        answer: [
+          'You should arrive before 8 pm on Friday, February 25 for an opening ceremony and other activities. The contribution period will begin at 11 pm and will last until 11 am on Sunday, February 24. A more detailed schedule is available can be found on our website.',
+        ],
+      },
+      {
+        question: 'Who',
+        answer: [
+          'All university and college students of all skill levels are eligible to attend. We welcome a diverse mix of colleges, disciplines, and interests at our event, and encourage students from outside the Champaign-Urbana area to attend. There will be workshops and support from mentors if you are new to open source.',
+        ],
+      },
     ],
   },
   {
-    question: 'When and where is HackIllinois?',
-    answer: [
-      'HackIllinois will be held from February 25-27th virtually on a Discord Server through which attendees can ask questions and interact with one another.',
+    sectionTitle: 'Before',
+    sectionFaqs: [
+      {
+        question: 'How do HackIllinois applications work?',
+        answer: [
+          'You must apply in order to be considered for HackIllinois. Admissions will be based on a weighted random lottery. Decisions will be released in early February in multiple waves, and it is to your advantage to apply as soon as possible. If you’re accepted, there will be a limited time to RSVP before we reallocate your spot to another student, so be on the lookout for your acceptance email!',
+        ],
+      },
+      {
+        question: 'How do I get there?',
+        answer: [
+          'HackIllinois will happen virtually this year due to COVID-19 guidelines and for everyone’s safety. All you will need for this year’s hackathon is: A Github account to share and collaborate on your code with others; A Discord account to work and engage with teammates, mentors, and other attendees.',
+        ],
+      },
+      {
+        question: 'Do I need a team?',
+        answer: [
+          'Due to the nature of HackIllinois as an open source event, you are encouraged to work with others, including mentors, to contribute to open source projects. In particular, your contribution is required to have a license to be eligible for a prize. Mentors will be available on Slack before the event, as well at a project session on Friday. There are no team size limits, but most contributions end up coming from teams of around 6-8.',
+        ],
+      },
+      {
+        question: 'What should I bring?',
+        answer: [
+          'You should bring a student ID, a reusable water bottle, a change of clothing, personal items such as toiletries, and a laptop & charger. Due to safety considerations, please do not bring desktop computers, extra monitors, weapons, or alcoholic beverages.',
+        ],
+      },
     ],
   },
   {
-    question: 'How do I register?',
-    answer: [
-      'We will be releasing applications on January 21, 2022.',
+    sectionTitle: 'During',
+    sectionFaqs: [
+      {
+        question: 'Who do I work with?',
+        answer: [
+          'Mentors from a variety of open source projects will work with you to learn about open source and make contributions. Project matching will occur primarily over Slack so watch out for details once you have RSVPd.',
+        ],
+      },
+      {
+        question: 'Is there anything to do other than code?',
+        answer: [
+          'ABSOLUTELY! We’ll be running a variety of mini-events this year, including our customary Nerf battles as well as some new events. We’ll have open source keynotes and engaging workshops as well, so definitely check out the schedule to be posted here on our website.',
+        ],
+      },
+      {
+        question: 'How can I contact a mentor?',
+        answer: [
+          'Room locations for each mentor will be posted on the website and mobile apps. Otherwise, you can contact them on Slack.',
+        ],
+      },
+      {
+        question: 'Do I have to go to the opening/ending ceremony?',
+        answer: [
+          'Yes! Prize and general important information will be covered at the opening ceremony. Prizes will be announced at the ending ceremony.',
+        ],
+      },
     ],
   },
   {
-    question: 'When does registration close?',
-    answer: [
-      'Interested participants can register until the day of the event, February 25, 2022.',
-    ],
-  },
-  {
-    question: 'Who can register?',
-    answer: [
-      'Anyone 18 years or older and enrolled in a high school, college, or university (or left school during the current school year) can register!',
-    ],
-  },
-  {
-    question: 'Do I need a team? How do I find one?',
-    answer: [
-      'No, you are not required to have a team to participate. With that being said, we strongly encourage you to work with other participants in order to get the full experience at HackIllinois. You can make teams prior to the event or find members through our team matching event (As part of Hackathons Crash Course/Hackathons 101) or on the Discord.',
-    ],
-  },
-  {
-    question: 'What should I bring?',
-    answer: [
-      'Nothing! Everything will be online!',
-    ],
-  },
-  {
-    question: 'Will there be food?',
-    answer: [
-      'Unfortunately, we won’t be able to provide meals or meal vouchers as our event is fully virtual.',
-    ],
-  },
-  {
-    question: 'Is there anything to do other than code?',
-    answer: [
-      'Yes! There will be a variety of mini-events, workshops, and company events for attendees to join. You should definitely check out the schedule that will be posted here on the website!',
-    ],
-  },
-  {
-    question: 'Do I need to have a programming background to participate?',
-    answer: [
-      'No! People of all skill levels are welcomed!',
-    ],
-  },
-  {
-    question: 'How can I stay updated with what is going on at the event?',
-    answer: [
-      'Please download the HackIllinois app (available shortly before the event on the App Store and Play Store)! Our website will also be continually updated with new information throughout the year.',
-    ],
-  },
-  {
-    question: 'Have more questions?',
-    answer: [
-      'Please don’t hesitate to reach out to us at contact@hackillinois.org if you have any other questions!',
+    sectionTitle: 'After',
+    sectionFaqs: [
+      {
+        question: 'Will there be prizes?',
+        answer: [
+          'Yes, there will be overall prizes as well as category prizes sponsored by companies split based on the type of contribution made. We encourage you to work on a project that interests you rather than aiming for a specific prize. Judging will be based on technical merit of the contribution, and not on flashiness or APIs used. Detailed prize information will be released closer to the event.',
+        ],
+      },
+      {
+        question: 'Are there similar events to this?',
+        answer: [
+          'Yes! Follow Illini Hackers on Facebook and Twitter to stay up-to-date on HackNights, college hackathons, and other events!',
+        ],
+      },
+      {
+        question: 'Have more questions?',
+        answer: [
+          'Please don’t hesitate to reach out to us at contact @hackillinois.org if you have any other questions.',
+        ],
+      },
     ],
   },
 ];
 
-export default FAQS;
+export default faqs;
