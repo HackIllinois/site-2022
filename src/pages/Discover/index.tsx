ef7ab075e2fe6fd3a0af1123f0622c9e651fc453
Felicia Wang <felicia5@illinois.edu>
2022-02-09 20:59:27 -0600

    Formatting changes

--- src/pages/Discover/index.tsx
+++ src/pages/Discover/index.tsx
@@ -8,18 +8,18 @@ import BLACKBOARD_HOVER from 'assets/discover/blackboard-hover.svg';
 import styles from './styles.module.scss';
 
 const Discover: React.FC = () => (
-    <div className={styles.discover}>
-        <div className={styles.background}>
-            <div className={styles.wall}/>
-            <div className={styles.floor_separator}/>
-            <img src={NEW_LOGO} alt="Logo" className={styles.logo} />
-            <img src={SHELF} alt="Pastry shelf" className={styles.shelf} />
-            <img src={DISPLAY} alt="Pastry display" className={styles.display} />
-            <img src={BLACKBOARD} alt="Blackboard" className={styles.blackboard}/>
-            <a href="/schedule"><img src={BLACKBOARD_HOVER} alt="Blackboard hover" className={styles.blackboard_hover}/></a>
+  <div className={styles.discover}>
+    <div className={styles.background}>
+      <div className={styles.wall} />
+      <div className={styles.floor_separator} />
+      <img src={NEW_LOGO} alt="Logo" className={styles.logo} />
+      <img src={SHELF} alt="Pastry shelf" className={styles.shelf} />
+      <img src={DISPLAY} alt="Pastry display" className={styles.display} />
+      <img src={BLACKBOARD} alt="Blackboard" className={styles.blackboard} />
+      <a href="/schedule"><img src={BLACKBOARD_HOVER} alt="Blackboard hover" className={styles.blackboard_hover} /></a>
 
-        </div>
     </div>
+  </div>
 );
 
 export default Discover;
