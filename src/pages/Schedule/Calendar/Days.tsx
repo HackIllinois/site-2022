dee89bd17df74ae8bd2b337b93bb102d5ea5f5af
estelle0500 <estelle0500@gmail.com>
2022-02-17 15:19:01 -0600

    Generalize calendar logic to work with any month

--- src/pages/Schedule/Calendar/Days.tsx
+++ src/pages/Schedule/Calendar/Days.tsx
@@ -9,59 +9,63 @@ type Props = {
   width: number
 };
 
-const startDate = 25;
-const endDate = 27;
+const startDate = new Date('February 25, 2022');
+const endDate = new Date('February 27, 2022');
+
+const firstDayOfMonth = new Date(startDate.getFullYear(), startDate.getMonth(), 1);
+const lastDayOfMonth = new Date(startDate.getFullYear(), startDate.getMonth() + 1, 0);
+
+const WIDTH_BREAKPOINT = 912;
 
 const Days = ({ date, setDate, width }: Props): JSX.Element => {
-  const [numOfWeeks, setNumOfWeeks] = useState(width > 950 ? 5 : 2);
-
-  useEffect(() => setNumOfWeeks(width > 950 ? 5 : 2), [width]);
+  const [shouldMinify, setShouldMinify] = useState(width <= WIDTH_BREAKPOINT);
+  useEffect(() => setShouldMinify(width <= WIDTH_BREAKPOINT), [width]);
 
   const changeDate = (d: number) => {
-    if (d >= startDate && d <= endDate) setDate(d);
+    if (d >= startDate.getDate() && d <= endDate.getDate()) setDate(d);
   };
 
   const daysInMonth = [];
-  for (let i = 0; i < 2; i++) {
-    daysInMonth.push(<td className={styles.empty} key={`startblank${i}`} />);
+  for (let i = 0; i < firstDayOfMonth.getDay(); i++) {
+    daysInMonth.push(<td className={styles.empty} key={`Blank ${i}`} />);
   }
-  for (let d = 1; d < 31; d++) {
+
+  for (let d = 1; d <= lastDayOfMonth.getDate(); d++) {
     const currentDay = (d === date);
-    const activeDay = (d >= startDate && d <= endDate);
+    const activeDay = (d >= startDate.getDate() && d <= endDate.getDate());
     daysInMonth.push(
       <td
         className={clsx(styles.calendarDay, currentDay && styles.selectedDay, activeDay && styles.activeDay)}
-        key={d}
+        key={`Day ${d}`}
         data-active={activeDay && !currentDay ? 'yes' : 'no'}
       >
         <button className={styles.dayButton} onClick={() => changeDate(d)} disabled={!activeDay}>{d}</button>
       </td>,
     );
   }
-  daysInMonth.push(<td className={styles.empty} key="endblank" />);
 
   const rows: JSX.Element[][] = [];
   let cells: JSX.Element[] = [];
+
   daysInMonth.forEach((day, i) => {
-    if (i % 7 !== 0) {
-      cells.push(day);
-      if (i === daysInMonth.length - 1) {
-        rows.push(cells);
-      }
-    } else {
+    cells.push(day);
+    if (cells.length === 7 || i === daysInMonth.length - 1) {
       rows.push(cells);
       cells = [];
-      cells.push(day);
     }
   });
 
+  const containsDate = (row: JSX.Element[], targetDate: Date) => (
+    row.some((cell) => cell.key === `Day ${targetDate.getDate()}`)
+  );
+
   return (
     <tbody>
-      {rows.map((d, i) => {
-        if (i > 1 && i <= numOfWeeks + 1) {
+      {rows.map((row, i) => {
+        if (!shouldMinify || containsDate(row, startDate) || containsDate(row, endDate)) {
           // array should be constant so
           // eslint-disable-next-line react/no-array-index-key
-          return <tr key={`daysinmonth${i}`}>{d}</tr>;
+          return <tr key={`Week ${i}`}>{row}</tr>;
         }
         return null;
       })}
