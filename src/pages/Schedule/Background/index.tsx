22364b333d7e289ba44c729d733a94ca76a9d31a
estelle0500 <estelle0500@gmail.com>
2022-02-17 14:29:31 -0600

    Lint

--- src/pages/Schedule/Background/index.tsx
+++ src/pages/Schedule/Background/index.tsx
@@ -7,10 +7,10 @@ import styles from './styles.module.scss';
 
 const Background: React.FC = () => (
   <div className={styles.background}>
-    <div className={styles.wall}/>
-    <div className={styles.floor_separator}/>
+    <div className={styles.wall} />
+    <div className={styles.floor_separator} />
     <img src={DISPLAY_HALF} alt="Pastry display" className={styles.display_half} />
-    <img src={DISPLAY_FULL} alt="Pastry display" className={styles.display_full}/>
+    <img src={DISPLAY_FULL} alt="Pastry display" className={styles.display_full} />
     <img src={SHELF} alt="Shelf" className={styles.shelf} />
   </div>
 );
