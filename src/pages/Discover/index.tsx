cbacf3e66be3052ab7780163f743b4def7d818d0
estelle0500 <estelle0500@gmail.com>
2022-02-17 21:52:10 -0600

    Add nav bar to discover page

--- src/pages/Discover/index.tsx
+++ src/pages/Discover/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 
+import NavBar from 'components/NavBar';
 import SHELF from 'assets/discover/shelf.svg';
 import DISPLAY from 'assets/discover/display.svg';
 import BLACKBOARD from 'assets/discover/blackboard.svg';
-import NEW_LOGO from 'assets/discover/new_logo.svg';
 import BLACKBOARD_HOVER from 'assets/discover/blackboard-hover.svg';
 import styles from './styles.module.scss';
 
 const Discover: React.FC = () => (
   <div className={styles.discover}>
+    <NavBar showHome />
+
     <div className={styles.background}>
       <div className={styles.wall} />
       <div className={styles.floor_separator} />
-      <img src={NEW_LOGO} alt="Logo" className={styles.logo} />
       <img src={SHELF} alt="Pastry shelf" className={styles.shelf} />
       <img src={DISPLAY} alt="Pastry display" className={styles.display} />
       <img src={BLACKBOARD} alt="Blackbnoard" className={styles.blackboard} />
