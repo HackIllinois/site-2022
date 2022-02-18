22681cfcf4bd93f42ba24403326ede99b3c21228
Estelle <estelle0500@gmail.com>
2022-02-17 21:58:31 -0600

    Add navigation bar (#10)
    * Add navigation bar back
    
    * Replace sidebar with dropdown menu
    
    * Add close icon
    
    * Add option to disable link highlight
    
    * Combine logo display logic
    
    * Add styles for mobile
    
    * Clean up unused variable
    
    * Fix padding
    
    * Move register button to nav bar
    
    * Add link to interactive journey
    
    * Add nav bar to discover page

--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import BACKGROUND from 'assets/home/hero/background.svg';
 import BAKERY from 'assets/home/hero/bakery.svg';
 import CLOUDS from 'assets/home/hero/clouds.svg';
-import LOGO from 'assets/home/hero/logo.svg';
 import PLANTS from 'assets/home/hero/plants.svg';
 import SCOOTER from 'assets/home/hero/scooter.svg';
 import SUN_RAYS from 'assets/home/hero/sun_rays.svg';
 
-// import NavBar from 'components/NavBar';
+import NavBar from 'components/NavBar';
 import styles from './styles.module.scss';
 
 const Section1: React.FC = () => (
   <div className={styles.hero}>
-    { // <NavBar hideLogo />
-    }
+    <NavBar showRegister />
     <div className={styles.background} style={{ backgroundImage: `url(${BACKGROUND})` }} />
     <div className={styles.clouds} style={{ backgroundImage: `url(${CLOUDS})` }} />
     <div className={styles.sun_rays} style={{ backgroundImage: `url(${SUN_RAYS})` }} />
-    <div className={styles.logo} style={{ backgroundImage: `url(${LOGO})` }} />
-
-    <Link className={styles.registerButton} to="/register">Register</Link>
 
     <div className={styles.foreground}>
       <img className={styles.plants} src={PLANTS} alt="Small plants" />
