504cb6d554cb0ca86e174a08982437ccde6ed42c
Aniket Gargya <gargya.aniket@gmail.com>
2022-02-20 20:53:57 -0600

    Redesign landing page (#18)
    * Update metadata
    
    * Fix path to preview image
    
    * Change meta content to absolute URLs
    
    * Fix gap
    
    * finish bare landing page
    
    * responsive cake
    
    * merge commits fix
    
    * reduce frosting hanging
    
    * fix hero background
    
    * add hero and cake spacing
    
    * fix build errors
    
    * make cake round
    
    * move cake down (on top of cake stand)
    
    * :iphone: responsive margin for cake stand
    
    * change top of event info padding to be responsive
    
    * :speech_balloon: replace eventinfo dummy text with real text
    
    * :lipstick: add imc to sponsor board
    
    * :fire: only show good code
    
    * :bug: remove unused imports
    
    * :lipstick: update eventinfo text, add insta disc twitter logos
    
    * :sparkles: add link to imc website
    
    * :building_construction: extract faq into json object
    
    * :fire: delete old sponsors code
    
    * :lipstick: add sponsor card to ui
    
    * :iphone: change sponsors section to use responsive padding
    
    * :bug: commit display card
    
    * :lipstick: add footer
    
    * :rocket: show footer
    
    * :bug: add key to eventinfo faq
    
    * :white_check_mark: use non-(array index) attribute for key
    
    * Update event blurb and FAQ for virtual event
    
    * Add more sponsors
    
    * Fix sponsor display using scalable middle section
    
    * Add more sponsor logos
    
    * Clean up
    
    * Increase widths of text containers on mobile
    
    * Fix FAQ answer
    
    * Fix trailing comma
    
    * :lipstick: emphasize certain words in event info
    
    * :lipstick: change color of event info to brown
    
    * :bento: fix instagram svg
    
    * :children_crossing: implement 3 column ui for join us
    
    * :iphone: switch 3 col layout to 1 col for small screens
    
    * :sparkles: add cupcake section (without carousel)
    
    * :construction: add speaker section updates
    
    * :rocket: show sponsors and hide speakers
    
    * :sparkles: make general faqs using piping thing
    
    * :speech_balloon: update faqs and fix piping sizing accordingly
    
    * :dizzy: add piping bag animation
    
    * Increase section header size on mobile
    
    * Revert "Merge branch 'dev' into landing-page"
    
    This reverts commit 6d132e816c9d10b54106fb8c11f4e1253dbe8d25, reversing
    changes made to 038e0476be02540f4efe0182a9b7bd4d8658485c.
    
    Co-authored-by: estelle0500 <estelle0500@gmail.com>

--- src/pages/Home/EventInfo/index.tsx
+++ src/pages/Home/EventInfo/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import lottie from 'lottie-web';
 
 import CAKE_LAYER_ONE from 'assets/home/event_info/cake_layer_one.svg';
 import CAKE_LAYER_TWO from 'assets/home/event_info/cake_layer_two.svg';
@@ -9,68 +10,104 @@ import INSTAGRAM_LOGO from 'assets/home/event_info/instagram_logo.svg';
 import DISCORD_LOGO from 'assets/home/event_info/discord_logo.svg';
 import TWITTER_LOGO from 'assets/home/event_info/twitter_logo.svg';
 
-import FAQS from './faqs';
+import PIPING_ANIMATION from 'assets/home/event_info/piping.json';
+
+import faqs from './faqs';
 
 import styles from './styles.module.scss';
 
-const EventInfo: React.FC = () => (
-  <section className={styles.eventInfo}>
-    <div className={styles.cakeLayer1}>
-      <div className={styles.cakeTop}>
-        <img className={styles.cakeFrosting} src={CAKE_LAYER_ONE} alt="" />
-        <h1>The Event</h1>
-      </div>
-      <div className={styles.cakeMiddle}>
-        <p>HackIllinois is the University of Illinois at Urbana-Champaign’s premiere collegiate hackathon. In recent years, we have run our first fully virtual hackathon as well as our very new Open Source Fellowship.</p>
-        <p>This year, participants will take part virtually at HackIllinois. Participants can work individually or in teams to submit projects to a specific track for a chance to win the prize.</p>
-      </div>
-      <div className={styles.cakeBottom} />
-    </div>
-    <div className={styles.cakeLayer2}>
-      <div className={styles.cakeTop}>
-        <img className={styles.cakeFrosting} src={CAKE_LAYER_TWO} alt="" />
-        <h1>Join Us</h1>
-      </div>
-      <div className={styles.cakeMiddle}>
-        <p>Follow us on Twitter (<a href="https://twitter.com/HackIllinois">@HackIllinois</a>) and Instagram (<a href="https://instagram.com/HackIllinois">@HackIllinois</a>) to be notified of our event updates!</p>
-        <p>As the event is fully virtual, we’ll connect mentors and attendees on Discord!</p>
-        <div className={styles.platformTitle}>
-          <img src={DISCORD_LOGO} alt="Discord Logo" />
-          <h2>Discord</h2>
+const EventInfo: React.FC = () => {
+  const [faqSectionIndex, setFaqSectionIndex] = useState(0);
+  const piping = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (piping.current) {
+      lottie.destroy();
+      lottie.loadAnimation({
+        container: piping.current,
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        animationData: PIPING_ANIMATION,
+      });
+    }
+  }, [faqSectionIndex]);
+
+  return (
+    <section className={styles.eventInfo}>
+      <div className={styles.cakeLayer1}>
+        <div className={styles.cakeTop}>
+          <img className={styles.cakeFrosting} src={CAKE_LAYER_ONE} alt="" />
+          <h1>The Event</h1>
         </div>
-        <p>Our discord server will be the main hub for our event. You’ll use this to find and communicate with teammates and other attendees, access our workshops and company events, and get help from mentors—all through one centralized platform.</p>
-        <div className={styles.platformTitle}>
-          <img src={TWITTER_LOGO} alt="Twitter Logo" />
-          <h2>Twitter</h2>
+        <div className={styles.cakeMiddle}>
+          <p><span className={styles.emphasized}>HackIllinois</span> is University of Illinois at Urbana-Champaign’s premiere collegiate hackathon. In recent years, we have run our first fully virtual hackathon as well as our very new Open Source Fellowship.</p>
+          <p>This year, participants will take part virtually at HackIllinois. Participants can work individually or in teams to submit projects to a specific track for a chance to win the prize.</p>
         </div>
-        <p>This is where we’ll be posting main updates with regards to our schedule, additions to our event, and other things we come up with! We’ll also be using it during the event to make updates with any schedule changes and send out reminders for workshops and other mini-events starting out!</p>
-        <div className={styles.platformTitle}>
-          <img src={INSTAGRAM_LOGO} alt="Instagram Logo" />
-          <h2>Instagram</h2>
+        <div className={styles.cakeBottom} />
+      </div>
+      <div className={styles.cakeLayer2}>
+        <div className={styles.cakeTop}>
+          <img className={styles.cakeFrosting} src={CAKE_LAYER_TWO} alt="" />
+          <h1>Join Us</h1>
         </div>
-        <p>This is where we’ll be posting more of the fun, behind-the-scenes aspects of our event! You’ll get an opportunity to see the work that goes into making the event a possibility through insights from teams and profiles of staff members.</p>
+        <div className={styles.cakeMiddle}>
+          <p>Follow us on <span className={styles.emphasized}>Twitter (<a href="https://twitter.com/HackIllinois">@HackIllinois</a>)</span> and <span className={styles.emphasized}>Instagram (<a href="https://instagram.com/HackIllinois">@HackIllinois</a>)</span> to be notified of our event updates!</p>
+          <p>As the event is fully virtual, we’ll connect mentors and attendees on Discord!</p>
+          <div className={styles.platforms}>
+            <div className={styles.platform}>
+              <div className={styles.platformTitle}>
+                <img src={DISCORD_LOGO} alt="Discord Logo" />
+                <h2>Discord</h2>
+              </div>
+              <p className={styles.platformDescription}>Our discord server will be the main hub for our event. You’ll use this to find and communicate with teammates and other attendees, access our workshops and company events, and get help from mentors—all through one centralized platform.</p>
+            </div>
+            <div className={styles.platform}>
+              <div className={styles.platformTitle}>
+                <img src={TWITTER_LOGO} alt="Twitter Logo" />
+                <h2>Twitter</h2>
+              </div>
+              <p className={styles.platformDescription}>This is where we’ll be posting main updates with regards to our schedule, additions to our event, and other things we come up with! We’ll also be using it during the event to make updates with any schedule changes and send out reminders for workshops and other mini-events starting out!</p>
+            </div>
+            <div className={styles.platform}>
+              <div className={styles.platformTitle}>
+                <img src={INSTAGRAM_LOGO} alt="Instagram Logo" />
+                <h2>Instagram</h2>
+              </div>
+              <p className={styles.platformDescription}>This is where we’ll be posting more of the fun, behind-the-scenes aspects of our event! You’ll get an opportunity to see the work that goes into making the event a possibility through insights from teams and profiles of staff members.</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.cakeBottom} />
       </div>
-      <div className={styles.cakeBottom} />
-    </div>
-    <div className={styles.cakeLayer3}>
-      <div className={styles.cakeTop}>
-        <img className={styles.cakeFrosting} src={CAKE_LAYER_THREE} alt="" />
-        <h1>General FAQs</h1>
-      </div>
-      <div className={styles.cakeMiddle}>
-        {FAQS.map((FAQ) => (
-          <React.Fragment key={FAQ.question}>
-            <h3>{FAQ.question}</h3>
-            {FAQ.answer.map((answer) => (
-              <p key={answer}>{answer}</p>
+      <div className={styles.cakeLayer3}>
+        <div className={styles.cakeTop}>
+          <img className={styles.cakeFrosting} src={CAKE_LAYER_THREE} alt="" />
+          <h1>General FAQs</h1>
+        </div>
+        <div className={styles.cakeMiddle}>
+          <div className={styles.faqSectionHeaders}>
+            {Array(faqs.length).fill(0).map((_, i) => i).map((i) => (
+              <div className={styles.faqSectionHeaderContainer}>
+                <button key={i} className={styles.faqSectionHeader} onClick={() => setFaqSectionIndex(i)}>{faqs[i].sectionTitle}</button>
+                {i === faqSectionIndex && <div ref={piping} className={styles.faqSectionPiping} />}
+              </div>
             ))}
-          </React.Fragment>
-        ))}
+          </div>
+          <div className={styles.faqContent}>
+            {faqs[faqSectionIndex].sectionFaqs.map(({ question, answer }) => (
+              <div className={styles.faqContentColumn}>
+                <h3>{question}</h3>
+                {answer.map((a) => (<p>{a}</p>))}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className={styles.cakeBottom} />
       </div>
-      <div className={styles.cakeBottom} />
-    </div>
-    <img className={styles.cakeStand} src={CAKE_STAND} alt="" />
-  </section>
-);
+      <img className={styles.cakeStand} src={CAKE_STAND} alt="" />
+    </section>
+  );
+};
 
 export default EventInfo;
