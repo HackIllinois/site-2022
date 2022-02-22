962cc0d5bd2dab8442112e826ebd1c77baa10dd7
Felicia Wang <felicia5@illinois.edu>
2022-02-22 17:52:42 -0600

    Create normal and async buttons

--- src/pages/Schedule/EventDisplay/index.tsx
+++ src/pages/Schedule/EventDisplay/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DateTime } from 'luxon';
 
 import Events from './Events';
@@ -18,11 +18,32 @@ const timezoneText = <span className={styles.timezoneText} title="automagically 
 
 const EventDisplay = ({ defaultDate, width }: Props): JSX.Element => {
   const [date, setDate] = useState(defaultDate);
+  const [selectedNormal, setSelectedNormal] = useState(true);
+  const [selectedAsync, setSelectedAsync] = useState(false);
   const d = new Date(`February ${date}, 2022`);
   const day = days[d.getDay()];
 
+  const selectNormal = () => {
+    setSelectedNormal(true);
+    setSelectedAsync(false);
+  };
+
+  const selectAsync = () => {
+    setSelectedNormal(false);
+    setSelectedAsync(true);
+  };
+
+  useEffect(() => {
+    setSelectedNormal(true);
+    setSelectedAsync(false);
+  }, []);
+
   return (
     <div className={styles.eventDisplay}>
+      <div className={styles.buttonContainer}>
+        <button className={selectedNormal === true ? styles.selectedButton : styles.unselectedButton} onClick={selectNormal}>Normal</button>
+        <button className={selectedAsync === true ? styles.selectedButton : styles.unselectedButton} onClick={selectAsync}>Async</button>
+      </div>
       <p className={styles.dayHeading}>February {date} - {day} {timezoneText}</p>
       <div className={styles.container}>
         <Calendar date={date} setDate={setDate} width={width} />
